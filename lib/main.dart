69c3bd9f4df07db8b3beef11b6db62f07e2b2fc9
Delvin Lim <tkjpublic@gmail.com>
2023-10-10 21:21:54 +0700

    feat: onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
-import 'package:konekto/widgets/appbar/app_bar_widget.dart';
+import 'package:konekto/pages/home/onboarding_page.dart';
+// import 'package:konekto/widgets/appbar/app_bar_widget.dart';
 
 /// Flutter code sample for [CupertinoPageScaffold].
 
-void main() => runApp(const KonektoTabBarApp());
+void main() => runApp(const OnboardingPage());
---
+++ lib/pages/home/onboarding_page.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cupertino_onboarding/cupertino_onboarding.dart';
+import 'package:konekto/pages/home/home_page.dart';
+import 'package:konekto/widgets/appbar/app_bar_widget.dart';
+
+/// Flutter code sample for [CupertinoPageScaffold].
+
+// void main() => runApp(const OnboardingPage());
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: OnBoarding(),
+    );
+  }
+}
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({super.key});
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return CupertinoOnboarding(
+      // color: Colors.white,
+      bottomButtonColor: Colors.lightBlue,
+      onPressedOnLastPage: () => Navigator.pushReplacement(context,
+          CupertinoPageRoute(builder: (context) => const KonektoTabBar())),
+      // onPressedOnLastPage: () => Navigator.of(context).pushNamedAndRemoveUntil(CupertinoPageRoute(builder: (context) => const KonektoTabBar()), (route) => false),
+
+      bottomButtonChild: const Text(
+        'Continue',
+        style: TextStyle(color: Colors.white),
+      ),
+      pages: [
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset('images/onboarding_1.png'),
+                ],
+              ),
+
+              // const SizedBox(height: 60.0),
+              const Center(
+                child: Text(
+                  'Discover',
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              const Center(
+                child: Text(
+                  'New Activities',
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              const SizedBox(height: 14.0),
+              const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Fostering meaningful relationships with individuals who have similar interests',
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset('images/onboarding_2.png'),
+                ],
+              ),
+
+              // const SizedBox(height: 60.0),
+              const Center(
+                child: Text(
+                  'Endless Fun and',
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              const Center(
+                child: Text(
+                  'Entertainment',
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              const SizedBox(height: 14.0),
+              const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Dive into a world of endless fun and entertainment with a diverse range of activities',
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset('images/onboarding_3.png'),
+                ],
+              ),
+
+              // const SizedBox(height: 60.0),
+              const Center(
+                child: Text(
+                  'Laugh and Share',
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              const Center(
+                child: Text(
+                  'Moments',
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              const SizedBox(height: 14.0),
+              const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Fostering meaningful relationships with individuals who have similar interests',
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
