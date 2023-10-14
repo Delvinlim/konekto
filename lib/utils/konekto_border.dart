58a6cf154824d4e1d53b05319d92dfb9b70d7238
Delvin Lim <tkjpublic@gmail.com>
2023-10-14 22:45:04 +0700

    feat: add workaround utilities for *CupertinoTextFormFieldRow* bug - issue #120360

---
+++ lib/utils/konekto_border.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: overridden_fields, annotate_overrides
+
+import 'package:flutter/cupertino.dart';
+
+class KonektoBorder extends Border {
+  const KonektoBorder({
+    this.top = BorderSide.none,
+    this.right = BorderSide.none,
+    this.bottom = BorderSide.none,
+    this.left = BorderSide.none,
+  });
+
+  const KonektoBorder.fromBorderSide(BorderSide side)
+      : top = side,
+        right = side,
+        bottom = side,
+        left = side;
+
+  factory KonektoBorder.all({
+    Color color = const Color(0xFF000000),
+    double width = 1.0,
+    BorderStyle style = BorderStyle.solid,
+    double strokeAlign = BorderSide.strokeAlignInside,
+  }) {
+    final BorderSide side = BorderSide(
+        color: color, width: width, style: style, strokeAlign: strokeAlign);
+    return KonektoBorder.fromBorderSide(side);
+  }
+
+  @override
+  EdgeInsetsGeometry get dimensions => const EdgeInsets.all(8.0);
+
+  final BorderSide top;
+
+  /// The right side of this border.
+  final BorderSide right;
+
+  final BorderSide bottom;
+
+  /// The left side of this border.
+  final BorderSide left;
+}
