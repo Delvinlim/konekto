a1705cb3215ea3ec5eefd6896f9a21f3e0fdc672
Delvin Lim <tkjpublic@gmail.com>
2023-11-12 04:07:58 +0700

    feat: home screen

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
+import 'package:konekto/pages/auth/forget_password_page.dart';
+import 'package:konekto/pages/notifications/notification_page.dart';
+import 'package:konekto/utils/konekto_border.dart';
+import 'package:konekto/widgets/card/event_card_widget.dart';
 
 /// Flutter code sample for [CupertinoPageScaffold].
 
@@ -23,33 +29,318 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
-  int _count = 0;
+Widget homeSlider = SizedBox(
+  height: 200,
+  child: Swiper(
+    itemBuilder: (BuildContext context, int index) {
+      return Image.network(
+        "https://via.placeholder.com/320x200",
+        fit: BoxFit.fill,
+      );
+    },
+    itemCount: 5,
+    pagination: const SwiperPagination(),
+    control: const SwiperControl(),
+  ),
+);
 
+Widget homeCommunityCategories = SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 64,
+            height: 64,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                border: KonektoBorder.all(color: Colors.grey.shade400),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: const Color(0xffD9D9D9)),
+            child: Image.asset(
+              'assets/images/football.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+          Container(
+            width: 64,
+            height: 64,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                border: KonektoBorder.all(color: Colors.grey.shade400),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: const Color(0xffD9D9D9)),
+            child: Image.asset(
+              'assets/images/football.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+          Container(
+            width: 64,
+            height: 64,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                border: KonektoBorder.all(color: Colors.grey.shade400),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: const Color(0xffD9D9D9)),
+            child: Image.asset(
+              'assets/images/football.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+          Container(
+            width: 64,
+            height: 64,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                border: KonektoBorder.all(color: Colors.grey.shade400),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: const Color(0xffD9D9D9)),
+            child: Image.asset(
+              'assets/images/football.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+          Container(
+            width: 64,
+            height: 64,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                border: KonektoBorder.all(color: Colors.grey.shade400),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: const Color(0xffD9D9D9)),
+            child: Image.asset(
+              'assets/images/football.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+          Container(
+            width: 64,
+            height: 64,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                border: KonektoBorder.all(color: Colors.grey.shade400),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: const Color(0xffD9D9D9)),
+            child: Image.asset(
+              'assets/images/football.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+          Container(
+            width: 64,
+            height: 64,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                border: KonektoBorder.all(color: Colors.grey.shade400),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: const Color(0xffD9D9D9)),
+            child: Image.asset(
+              'assets/images/football.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+          Container(
+            width: 64,
+            height: 64,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+                border: KonektoBorder.all(color: Colors.grey.shade400),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: const Color(0xffD9D9D9)),
+            child: Image.asset(
+              'assets/images/football.png',
+              height: 50.0,
+              width: 50.0,
+            ),
+          ),
+        ]));
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       // Uncomment to change the background color
       // backgroundColor: CupertinoColors.systemPink,
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('Home'),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Text('You have pressed the button $_count times.'),
-            ),
-            const SizedBox(height: 20.0),
-            Center(
-              child: CupertinoButton.filled(
-                onPressed: () => setState(() => _count++),
-                child: const Icon(CupertinoIcons.add),
+      // navigationBar: const CupertinoNavigationBar(
+      //   middle: Text('Home'),
+      // ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 40.0, horizontal: 10.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50.0),
+                          child: Image.asset(
+                            'assets/images/profile.png',
+                            height: 50.0,
+                            width: 50.0,
+                          ),
+                        ),
+                      ),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Good Morning,',
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.w400),
+                          ),
+                          Text(
+                            'Delvin Lim',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ],
               ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 40.0, horizontal: 10.0),
+                        child: CupertinoButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) =>
+                                        const NotificationsPage()));
+                          },
+                          child: const Icon(
+                            CupertinoIcons.bell,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          homeSlider,
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Categories',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) =>
+                                        const ForgetPasswordPage()));
+                          },
+                          child: const Text(
+                            'See All',
+                            style: TextStyle(color: Colors.black87),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          homeCommunityCategories,
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Event',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                // Column(
+                //   children: [
+                //     Row(
+                //       children: [
+                //         GestureDetector(
+                //           onTap: () {
+                //             Navigator.push(
+                //                 context,
+                //                 CupertinoPageRoute(
+                //                     builder: (context) =>
+                //                         const ForgetPasswordPage()));
+                //           },
+                //           child: const Text(
+                //             'See All',
+                //             style: TextStyle(color: Colors.black87),
+                //           ),
+                //         )
+                //       ],
+                //     ),
+                //   ],
+                // ),
+              ],
+            ),
+          ),
+          const EventCard(),
+        ],
       ),
     );
   }
---
+++ lib/widgets/card/event_card_widget.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:konekto/utils/konekto_border.dart';
+
+class EventCard extends StatefulWidget {
+  const EventCard({super.key});
+
+  @override
+  State<EventCard> createState() => _EventCardState();
+}
+
+class _EventCardState extends State<EventCard> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                  width: 200,
+                  // height: 170,
+                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(
+                    border: KonektoBorder.all(color: Colors.black),
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Column(
+                    children: [
+                      Positioned(
+                        child: Container(
+                          width: 200,
+                          height: 85,
+                          decoration: ShapeDecoration(
+                            image: const DecorationImage(
+                              image: AssetImage("assets/images/event_1.png"),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6)),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 12, bottom: 8),
+                        child: const Text(
+                          '2023 Kepri Cup',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      CupertinoButton(
+                          padding: const EdgeInsets.only(left: 24, right: 24),
+                          color: Colors.blue.shade600,
+                          child: const Text('View Details',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  // color: Colors.black,
+                                  fontWeight: FontWeight.bold)),
+                          onPressed: () {})
+                    ],
+                  )),
+              Container(
+                  width: 200,
+                  // height: 170,
+                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(
+                    border: KonektoBorder.all(color: Colors.black),
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Column(
+                    children: [
+                      Positioned(
+                        child: Container(
+                          width: 200,
+                          height: 85,
+                          decoration: ShapeDecoration(
+                            image: const DecorationImage(
+                              image: AssetImage("assets/images/event_2.png"),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6)),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 12, bottom: 8),
+                        child: const Text(
+                          'Batam Drawing',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      CupertinoButton(
+                          padding: const EdgeInsets.only(left: 24, right: 24),
+                          color: Colors.blue.shade600,
+                          child: const Text('View Details',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  // color: Colors.black,
+                                  fontWeight: FontWeight.bold)),
+                          onPressed: () {})
+                    ],
+                  )),
+              Container(
+                  width: 200,
+                  // height: 170,
+                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(
+                    border: KonektoBorder.all(color: Colors.black),
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Column(
+                    children: [
+                      Positioned(
+                        child: Container(
+                          width: 200,
+                          height: 85,
+                          decoration: ShapeDecoration(
+                            image: const DecorationImage(
+                              image: AssetImage("assets/images/event_3.png"),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6)),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 12, bottom: 8),
+                        child: const Text(
+                          'UIB Run',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      CupertinoButton(
+                          padding: const EdgeInsets.only(left: 24, right: 24),
+                          color: Colors.blue.shade600,
+                          child: const Text('View Details',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  // color: Colors.black,
+                                  fontWeight: FontWeight.bold)),
+                          onPressed: () {})
+                    ],
+                  )),
+            ]));
+  }
+}
