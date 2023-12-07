b4c606dac8b24ac8a0c2d9da6929d4390332f3b8
Delvin Lim <tkjpublic@gmail.com>
2023-12-07 23:31:39 +0700

    feat: transparent android statusbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:konekto/pages/home/onboarding_page.dart';
+import 'package:flutter/services.dart';
 // import 'package:konekto/widgets/appbar/app_bar_widget.dart';
 
 /// Flutter code sample for [CupertinoPageScaffold].
 
-void main() => runApp(const OnboardingPage());
+// void main() {
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(const OnboardingPage());
+}
