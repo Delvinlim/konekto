ae5d81450ed45cc36acb261213f21eda4341e7b2
Delvin Lim <tkjpublic@gmail.com>
2023-12-12 01:52:57 +0700

    refactor: add dynamic page per item support

--- lib/widgets/item/settings_item_widget.dart
+++ lib/widgets/item/settings_item_widget.dart
@@ -2,9 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsItem extends StatefulWidget {
-  const SettingsItem({super.key, required this.name, required this.icon});
+  const SettingsItem(
+      {super.key, required this.name, required this.icon, required this.page});
   final String name;
   final Icon icon;
+  final Widget page;
 
   @override
   State<SettingsItem> createState() => _SettingsItemState();
@@ -13,34 +15,41 @@ class SettingsItem extends StatefulWidget {
 class _SettingsItemState extends State<SettingsItem> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(6),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
+    return GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
+          Navigator.push(
+              context, CupertinoPageRoute(builder: (context) => widget.page));
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(6),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                  margin: const EdgeInsets.only(right: 8), child: widget.icon),
-              Text(
-                widget.name,
-                style: const TextStyle(
-                  color: CupertinoColors.black,
-                  fontSize: 16,
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.w700,
-                ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      margin: const EdgeInsets.only(right: 8),
+                      child: widget.icon),
+                  Text(
+                    widget.name,
+                    style: const TextStyle(
+                      color: CupertinoColors.black,
+                      fontSize: 16,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+              const Icon(
+                CupertinoIcons.chevron_right,
+                color: Colors.black,
               ),
             ],
           ),
-          const Icon(
-            CupertinoIcons.chevron_right,
-            color: Colors.black,
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
