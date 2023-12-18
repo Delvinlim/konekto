b28c3a6bc3f5ec526b8f85c6422b7526252b46b0
Delvin Lim <tkjpublic@gmail.com>
2023-12-19 01:26:05 +0700

    feat: terms and policies screen content

--- lib/pages/settings/terms_settings_page.dart
+++ lib/pages/settings/terms_settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:custom_accordion/custom_accordion.dart';
+import 'package:flutter/material.dart';
 
 class TermsSettingPage extends StatefulWidget {
   const TermsSettingPage({super.key});
@@ -22,6 +24,188 @@ class _TermsSettingState extends State<TermsSettingPage> {
             },
           ),
         ),
-        child: Container());
+        child: SafeArea(
+            child: Container(
+                padding: const EdgeInsets.all(12),
+                child: ListView(
+                  children: [
+                    CustomAccordion(
+                      title: 'Acceptance of Terms',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'User Eligibility',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'Community Guidelines',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'User-Generated Content',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'Privacy and Data',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'Intellectual Property',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'Termination',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'Disclaimer of Warranties',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'Limitation of Liability',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'Changes to Terms',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                    CustomAccordion(
+                      title: 'Governing Law',
+                      headerBackgroundColor: CupertinoColors.white,
+                      titleStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: CupertinoColors.black,
+                      ),
+                      toggleIconOpen: Icons.keyboard_arrow_down_sharp,
+                      toggleIconClose: Icons.keyboard_arrow_up_sharp,
+                      headerIconColor: CupertinoColors.black,
+                      accordionElevation: 2,
+                      widgetItems: const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi id quam finibus, sit amet cursus nunc ultrices.',
+                      ),
+                    ),
+                  ],
+                ))));
   }
 }
